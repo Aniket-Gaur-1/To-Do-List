83a17dcef2286c80b6065d81f61bc223d65d1425
Aniket-Gaur-1 <aniketgaur1149@outlook.com>
2023-12-13 15:21:18 +0530

    all done

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [mainTask, setMainTask] = useState([]);
+
+  function handleChange(e) {
+    setTask(e.target.value);
+
+    console.log(e.target.name, e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setMainTask([...mainTask].concat(task));
+
+    setTask("");
+    console.log(mainTask);
+    // setMainTask([]);
+  }
+  function deleteTask(i) {
+    const newtodo = mainTask.filter((_, taskindex) => taskindex !== i);
+    setMainTask(newtodo);
+  }
+  return (
+    <div className="box">
+      <form className="container" onSubmit={handleSubmit}>
+        (
+        <input
+          type="text"
+          placeholder="Enter a Task"
+          className="task"
+          name="title"
+          value={task}
+          onChange={handleChange}
+          id={task.length}
+        />
+        ? <button className="btn">Add Task</button>) : alert("PLease eneter
+        tasks")
+      </form>
+      <div>
+        <ul>
+          {mainTask.map((t, i) => (
+            <li>
+              <div>
+                <h2>{t}</h2>
+              </div>
+              <button className="delete_btn" onClick={() => deleteTask(i)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
